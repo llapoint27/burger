71169c7d08692444fc0e3e3dc11a7a0571cb13cb
Marco Polo <laurenlapoint@Laurens-MacBook-Pro.local>
2018-10-08 12:49:49 -0400

    initial folder/file push

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("double bacon cheese burger", true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("vegan blackbean burger", true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("jalapeno cheddar burger", true);
+
